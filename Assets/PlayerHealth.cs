ead3c5afa36612ac2f775883676bc06baebfce7d
aria184 <90838727+aria184@users.noreply.github.com>
2024-11-26 11:56:16 +0000

    Gaming health bar

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float maxHealth;
     public float currentHealth;
+
+    public float damage;
+    public float heal;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
